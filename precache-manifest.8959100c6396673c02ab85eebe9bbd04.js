a033128ac4e30cf273de139bd6de0d63e25db09a
unknown <harsharay.ray@gmail.com>
2020-05-27 13:10:17 +0530

    Updates

---
+++ precache-manifest.8959100c6396673c02ab85eebe9bbd04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b31eaa89d38fb9534b42ecf333b2183",
+    "url": "/Movie_Search/index.html"
+  },
+  {
+    "revision": "a4898160c2408a184e96",
+    "url": "/Movie_Search/static/css/main.9ea62b66.chunk.css"
+  },
+  {
+    "revision": "43544cb405b2be5fd03b",
+    "url": "/Movie_Search/static/js/2.280a9021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie_Search/static/js/2.280a9021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4898160c2408a184e96",
+    "url": "/Movie_Search/static/js/main.cce3dc03.chunk.js"
+  },
+  {
+    "revision": "27accd36a3f49e8fc7d1",
+    "url": "/Movie_Search/static/js/runtime-main.e0377dc2.js"
+  }
+]);
